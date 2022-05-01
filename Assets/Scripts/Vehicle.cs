5d367fa862023af0b39dcac3d90a6ca568f3c9b7
zar4za <zar4z4@yandex.ru>
2022-05-01 13:16:57 +0300

    Добавлены файлы проекта и тестовый скрипт авто

--- Assets/Scripts/Vehicle.cs
+++ Assets/Scripts/Vehicle.cs
@@ -4,15 +4,13 @@ using UnityEngine;
 
 public class Vehicle : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField]
+    private float _speed = 10;
 
-    // Update is called once per frame
-    void Update()
+    private void FixedUpdate()
     {
-        
+        Vector3 updatePosition = transform.position;
+        updatePosition.x += _speed * Time.fixedDeltaTime;
+        transform.position = updatePosition;
     }
 }
